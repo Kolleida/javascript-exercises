26b850d5d70e1ca3ef2f3bda1082665ea9b0361c
Eric S. Wang <ewang529@terpmail.umd.edu>
2024-01-10 13:20:53 -0500

    Complete fundamentals pt 4. Get practice in.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    const contains = function(arr, elt) {
+        for (const e of arr) {
+            if (elt === e) {
+                return true;
+            }
+        }
+        return false;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (contains(args, arr[i])) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (!Number.isInteger(n1) || !Number.isInteger(n2) || n1 < 0 || n2 < 0) {
+        return "ERROR";
+    }
+    return (Math.abs(n2 - n1) + 1) * (n1 + n2) / 2;
 };
 
 // Do not edit below this line
